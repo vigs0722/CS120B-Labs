89320ae3233a1088bb3994dbebcf65b7105e490c
UCR CSE <vigs0722@gmail.com>
2019-04-10 16:00:50 -0700

    init

---
+++ vsubr004_lab2_part3/vsubr004_lab2_part3/main.c
@@ -0,0 +1,42 @@
+/*
+ * vsubr004_lab2_part3.c
+ *
+ * Created: 4/10/2019 3:52:34 PM
+ * Author : ucrcse
+ */ 
+
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char cntavail = 0x00;
+	unsigned char tmpA;
+	while (1)
+	{
+		tmpA = PINA & 0x0F;
+		cntavail = 0x00;
+		if ((tmpA & 0x01) == 0x00) {
+			++cntavail;
+		}
+		
+		if ((tmpA & 0x02) == 0x00) {
+			++cntavail;
+		}
+		
+		if ((tmpA & 0x04) == 0x00) {
+			++cntavail;
+		}
+		
+		if ((tmpA & 0x08) == 0x00) {
+			++cntavail;
+		}
+		if(tmpA == 0x0F){
+			cntavail = 128;
+		}
+		PORTC = cntavail;
+	}
+}
