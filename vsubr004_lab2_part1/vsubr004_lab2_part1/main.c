cc73b6f18178f823fbb056efa331b96a63f0e9f6
vigs0722 <35055248+vigs0722@users.noreply.github.com>
2019-04-09 17:46:36 -0700

    first commit

---
+++ vsubr004_lab2_part1/vsubr004_lab2_part1/main.c
@@ -0,0 +1,39 @@
+/*
+ * vsubr004_lab2_part2.c
+ *
+ * Created: 4/9/2019 4:48:15 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char cntavail = 0x00;
+	unsigned char tmpA;
+	while (1)
+	{
+		tmpA = PINA & 0x0F;
+		cntavail = 0x00;
+		if ((tmpA & 0x01) == 0x00) {
+			++cntavail;
+		}
+		
+		if ((tmpA & 0x02) == 0x00) {
+			++cntavail;
+		}
+		
+		if ((tmpA & 0x04) == 0x00) {
+			++cntavail;
+		}
+		
+		if ((tmpA & 0x08) == 0x00) {
+			++cntavail;
+		}
+		PORTC = cntavail;
+	}
+}
