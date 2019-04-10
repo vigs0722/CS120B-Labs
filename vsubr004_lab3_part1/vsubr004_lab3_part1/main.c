bfa7a15f51fa6ce5850468578ec2d50928c3deee
vigs0722 <35055248+vigs0722@users.noreply.github.com>
2019-04-09 18:13:35 -0700

    create lab3 part 1 file

---
+++ vsubr004_lab3_part1/vsubr004_lab3_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * vsubr004_lab3_part1.c
+ *
+ * Created: 4/9/2019 6:11:01 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
