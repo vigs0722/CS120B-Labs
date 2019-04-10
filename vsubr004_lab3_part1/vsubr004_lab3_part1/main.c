295dc7ac6f7b1009f1b2fc46a353202481f103ac
vigs0722 <35055248+vigs0722@users.noreply.github.com>
2019-04-09 18:55:17 -0700

    second commit

--- vsubr004_lab3_part1/vsubr004_lab3_part1/main.c
+++ vsubr004_lab3_part1/vsubr004_lab3_part1/main.c
@@ -11,8 +11,37 @@
 int main(void)
 {
     /* Replace with your application code */
-    while (1) 
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+    unsigned char cnta;
+	unsigned char tempA; 
+	unsigned char cntb;
+	unsigned char tempB;
+	unsigned char num = 0x00;
+	unsigned char num2 = 0x00;
+	int a = 8;
+	int b = 8;
+	while (1) 
     {
-    }
+		tempA = PINA;
+		tempB = PINB;
+		while(a!=0){
+		num = tempA & 0x01;
+			if(num){
+				++cnta;
+			}
+		--a;
+		tempA >> 1;
+	    }
+		while(b!=0){
+			num2 = tempB & 0x01;
+			if(num2){
+				++cntb;
+			}
+			--b;
+			tempB >> 1;
+		}
+}
+PORTC = cnta+cntb;
 }
 
